ad65b954901eee67d94bbc3835f8666c9286eac8
HK <cmkbeew@naver.com>
2022-04-11 16:30:02 +0900

    test

--- java/team1/togather/fileset/Path.java
+++ java/team1/togather/fileset/Path.java
@@ -5,3 +5,5 @@ public class Path {
 	public static final String GALLERY_PHOTO = "C:/HK/6_Spring/sts-bundle/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp2/wtpwebapps/Togather/resources/assets/img/groupGallery/";
 	public static final String GBFILE_STORE = "C:/HK/6_Spring/sts-bundle/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp2/wtpwebapps/Togather/resources/assets/img/gbFile/";
 }
+
+//현기 git테스트
